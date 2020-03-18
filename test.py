893600d0f5af7f6cdc7cc5b021b382621cebd38e
Gemma <gemma.fu.sun@gmail.com>
2020-03-18 13:19:54 -0700

    Code for zig-zag.

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-username = input('Enter username:')
-password = input('Enter password:')
-print(f'You entered {username} (password={password})')
+import sys, time
+
+try:
+    indent = 0
+    indent_increase = True
+
+    while True:
+        print(' ' * indent, end='')
+        print('*' * 8)
+        time.sleep(0.1)
+
+        if indent_increase:
+            indent += 1
+            if indent == 10:
+                indent_increase = False
+        else:
+            indent -= 1
+            if indent == 0:
+                indent_increase = True
+	
+except KeyboardInterrupt:
+    sys.exit()
