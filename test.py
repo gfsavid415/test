ab5356657fb29653f024f5ce50566283a43f163f
Gemma <gemma.fu.sun@gmail.com>
2020-04-27 12:43:38 -0700

    Prints 'hello world'.

--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-""" 
-This code prints a back-and-forth zig-zag pattern.
-Author: Gemma
-Date: 04/23/2020
-"""
+def hello():
+    print('hello world')
 
-import sys, time
-
-try:
-    indent = 0
-    indent_increase = True
-
-    while True:
-        print(' ' * indent, end='')
-        print('*' * 10)
-        time.sleep(0.1)
-
-        if indent_increase:
-            indent += 1
-            if indent == 10:
-                indent_increase = False
-        else:
-            indent -= 1
-            if indent == 0:
-                indent_increase = True
-	
-except KeyboardInterrupt:
-    sys.exit()
+hello()
