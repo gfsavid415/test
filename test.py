f83f104057d6b50730fdd49b8ad9d0deeb939cdf
Gemma <gemma.fu.sun@gmail.com>
2020-05-01 11:58:28 -0700

    Print 'hello world'

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-# Date: 04/29/2020
-# Author: Gemma
+def hello():
+    print('hello world')
 
-def hello(): 
-    print('hello mundo <3 !!!')
-    print('So much to be GRATEFUL for!!!')	
-
-hello() 
+hello()
